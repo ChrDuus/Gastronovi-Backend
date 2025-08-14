a866a783ba1c1ae99a64b68b03c8c5bc781c2674
ChrDuus <duus92@web.de>
2025-08-14 13:41:59 +0200

    added sortation by category

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,34 +3,38 @@ import Product from '../models/productModel';
 
 class ProductController {
     public async getProducts(req: Request, res: Response): Promise<void> {
-        // Logic to retrieve products
-        res.send('List of products');
-    }
+    const products = await Product.find();
+    res.json(products);
+}
 
     public async getProductById(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
-        // Logic to retrieve a product by ID
         res.send(`Details of product with ID: ${id}`);
     }
 
-    public async createProduct(req: Request, res: Response): Promise<void> {
-        const productData = req.body;
-        // Logic to create a new product
-        res.status(201).send('Product created');
-    }
+   public async createProduct(req: Request, res: Response): Promise<void> {
+    const productData = req.body;
+    const product = new Product(productData);
+    await product.save();
+    res.status(201).json(product);
+}
 
     public async updateProduct(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
         const productData = req.body;
-        // Logic to update a product
         res.send(`Product with ID: ${id} updated`);
     }
 
     public async deleteProduct(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
-        // Logic to delete a product
         res.send(`Product with ID: ${id} deleted`);
     }
+
+    public async getProductsByCategory(req: Request, res: Response): Promise<void> {
+    const { category } = req.query;
+    const products = await Product.find(category ? { category } : {}).sort({ category: 1, name: 1 });
+    res.json(products);
+}
 }
 
 export default new ProductController();
